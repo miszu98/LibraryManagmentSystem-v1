59899d5a4d0452c1f277c2c1653ba1c97ebe9720
Michal Malek <michal.malek98@wp.pl>
2021-03-27 12:55:25 +0100

    almost final

--- LibraryManagmentSystemv1/Class.h
+++ LibraryManagmentSystemv1/Class.h
@@ -10,6 +10,7 @@ class User {
 		string email;
 		string password;
 		string phoneNumber;
+		string role;
 
 	public: 
 		string getFirstName() {
@@ -27,6 +28,9 @@ class User {
 		string getPhoneNumber() {
 			return phoneNumber;
 		}
+		string getRole() {
+			return role;
+		}
 
 		void setFirstName(string value) {
 			firstName = value;
@@ -40,8 +44,11 @@ class User {
 		void setPassword(string value) {
 			password = value;
 		}
-		int setPhoneNumber(string value) {
+		void setPhoneNumber(string value) {
 			phoneNumber = value;
+		}
+		void setRole(string value) {
+			role = value;
 		}
 
 		User() {
@@ -49,10 +56,74 @@ class User {
 		}
 
 		User(string _firstName, string _lastName, string _email, string _password, string _phoneNumber) {
-			firstName = _firstName;
-			lastName = _lastName;
-			email = _email;
-			password = _password;
-			phoneNumber = _phoneNumber;
+			setFirstName(_firstName);
+			setLastName(_lastName);
+			setEmail(_email);
+			setPassword(_password);
+			setPhoneNumber(_phoneNumber);
+			setRole("ROLE_USER");
+		}
+};
+
+class Book {
+	private:
+		string author;
+		string yearOfPublishment;
+		string title;
+		string typeOfBook;
+		string countBooks;
+
+	public:
+		string getAuthor() {
+			return author;
+		}
+		string getYearOfPublishment() {
+			return yearOfPublishment;
+		}
+		string getTitle() {
+			return title;
+		}
+		string getTypeOfBook() {
+			return typeOfBook;
+		}
+		string getCountBooks() {
+			return countBooks;
+		}
+
+		void setAuthor(string value) {
+			author = value;
+		}
+		void setYearOfPublishment(string value) {
+			yearOfPublishment = value;
+		}
+		void setTitle(string value) {
+			title = value;
+		}
+		void setTypeOfBook(string value) {
+			typeOfBook = value;
+		}
+		void setCountBook(string value) {
+			countBooks = value;
+		}
+
+		void rep_str()
+		{
+			cout << "Author: " << getAuthor() << endl;
+			cout << "YearOfPublishment: " << getYearOfPublishment() << endl;
+			cout << "Title: " << getTitle() << endl;
+			cout << "TypeOfBook: " << getTypeOfBook() << endl;
+			cout << "CountBooks: " << getCountBooks() << endl;
+		}
+
+		Book() {
+
+		}
+
+		Book(string _author, string _yearOfPublishment, string _title, string _typeOfBook, string _countBooks) {
+			setAuthor(_author);
+			setYearOfPublishment(_yearOfPublishment);
+			setTitle(_title);
+			setTypeOfBook(_typeOfBook);
+			setCountBook(_countBooks);
 		}
 };
--- LibraryManagmentSystemv1/LibraryManagmentSystemv1.cpp
+++ LibraryManagmentSystemv1/LibraryManagmentSystemv1.cpp
@@ -3,6 +3,7 @@
 #include "Class.h"
 #include <Windows.h>
 #include "Validators.h"
+#include <string>
 
 
 using namespace std;
@@ -11,13 +12,203 @@ void checkChoiceFromMainMenu(int choice);
 void loggedUser();
 void registerUser();
 void saveUserInDatabase(User user);
+void fetchAllBooks();
+void checkChoiceFromLoggedUser(int choice);
+void borrowBook(int id_book);
+bool checkCountOfBook(int id_book);
+void mainMenu();
+void adminMenu();
+void earlyScreen();
+void fetchAllUsers();
+void saveBook(Book book);
+void borrowBookViaAdmin();
+void addBook();
 
+int id_user;
 
 MYSQL* connection;
+string query;
 
+void signInAdmin()
+{   
+    string email, password;
+    cout << "\n\n";
+    cout << "                 ---------------------------------------------------" << endl;
+    cout << "                 ----Welcome in Library Managment System - ADMIN----" << endl;
+    cout << "                 ---------------------------------------------------" << endl;
+    cout << "                 ----                   Sign In                 ----" << endl;
+    cout << "                 ---------------------------------------------------" << endl;
+
+    while (email.length() == 0)
+    {
+        cout << "                                    E-mail: ";
+        cin >> email;
+    }
+
+    while (password.length() == 0)
+    {
+        cout << "                                    Password: ";
+        cin >> password;
+    }
+
+    query = "SELECT * FROM USERS WHERE ROLE='ROLE_ADMIN'";
+    mysql_query(connection, query.c_str());
+
+    MYSQL_RES* res = mysql_store_result(connection);
+    MYSQL_ROW row;
+        
+    while (row = mysql_fetch_row(res))
+    {
+            
+        if (row[3] == email && row[4] == password)
+            
+        {
+                id_user = *row[0] - '0';
+                adminMenu();
+                return;
+                break;
+        }
+        
+    }
+    cout << "                 User not found in database..." << endl;
+    earlyScreen();
+}
+
+void earlyScreen()
+{
+    cout << "\n\n";
+    cout << "                 -------------------------------------------" << endl;
+    cout << "                 ----Welcome in Library Managment System----" << endl;
+    cout << "                 -------------------------------------------" << endl;
+    cout << "                 ----                                   ----" << endl;
+    cout << "                 ----              1) ADMIN             ----" << endl;
+    cout << "                 ----              2) USER              ----" << endl;
+    cout << "                 ----              3) EXIT              ----" << endl;
+    cout << "                 -------------------------------------------" << endl;
+    
+
+    int choice = 0;
+    while (choice != 1 &&  choice != 2 && choice != 3)
+    {
+        cout << "                 >> ";
+        cin >> choice;
+    }
+
+    switch (choice)
+    {   
+        case 1:
+            signInAdmin();
+            break;
+
+        case 2:
+            mainMenu();
+            break;
+        case 3:
+            break;
+    }
+}
+
+void adminMenu()
+{
+    cout << "\n\n";
+    cout << "                 ---------------------------------------------------" << endl;
+    cout << "                 ----Welcome in Library Managment System - ADMIN----" << endl;
+    cout << "                 ---------------------------------------------------" << endl;
+    cout << "                 ----                                           ----" << endl;
+    cout << "                 ----               1) Add user                 ----" << endl;
+    cout << "                 ----               2) Display all users        ----" << endl;
+    cout << "                 ----               3) Add book                 ----" << endl;
+    cout << "                 ----               4) Borrow book              ----" << endl;
+    cout << "                 ----               5) Return book              ----" << endl;
+    cout << "                 ----               6) Logout                   ----" << endl;
+    cout << "                 ---------------------------------------------------" << endl;
+
+    int choice = 0;
+    while (choice != 1 && choice != 2 && choice != 3 && choice != 4 && choice != 5 && choice != 6)
+    {
+        cout << "                 >> ";
+        cin >> choice;
+    }
+
+    switch (choice)
+    {
+    case 0:
+        earlyScreen();
+        break;
+    case 1:
+        registerUser();
+        break;
+    case 2:
+        fetchAllUsers();
+        break;
+    case 3:
+        addBook();
+        break;
+    case 4:
+
+        break;
+    case 5:
+
+        break;
+    case 6:
+        earlyScreen();
+        break;
+    }
+}
+
+void borrowBookViaAdmin()
+{
+
+}
+
+void addBook()
+{
+    string author, title, typeOfBook, yearOfPublishment, countBook;
+    cout << "                                 Author: ";
+    cin.ignore();
+    getline(cin, author);
+    cout << "                                 Year Of Publishment (f.e 1874): ";
+    getline(cin, yearOfPublishment);
+    cout << "                                 Title: ";
+    getline(cin, title);
+    cout << "                                 Type of book: ";
+    getline(cin, typeOfBook);
+    cout << "                                 Number of copies: ";
+    getline(cin, countBook);
+
+    Book book = Book(author, yearOfPublishment, title, typeOfBook, countBook);
+    //book.rep_str();
+    saveBook(book);
+}
+
+void saveBook(Book book)
+{
+    query = "INSERT INTO BOOKS(author, yearOfPublishment, title, typeOfBook, countBooks) VALUES('" + book.getAuthor() + "', '"+book.getYearOfPublishment()+"', '" + book.getTitle() + "', '" + book.getTypeOfBook() + "', '"+book.getCountBooks()+"')";
+    mysql_query(connection, query.c_str());
+    cout << endl;
+    cout << "                                 Book added to database..." << endl;
+    cout << endl;   
+    adminMenu();
+}
+
+void fetchAllUsers()
+{
+    query = "SELECT * FROM USERS";
+    mysql_query(connection, query.c_str());
+
+    MYSQL_RES* res = mysql_store_result(connection);
+    MYSQL_ROW row;
+    cout << "USERS " << endl;
+    while (row = mysql_fetch_row(res))
+    {
+        cout << "                 " << row[0] << " " << row[1] << " " << row[2] << " " << row[3] << " " << row[5] << " " << row[6] << endl;
+    }
+
+
+}
 
 void mainMenu()
-{
+{   
     cout << "\n\n";
     cout << "                 -------------------------------------------" << endl;
     cout << "                 ----Welcome in Library Managment System----" << endl;
@@ -35,13 +226,21 @@ void mainMenu()
     cout << "                 -------------------------------------------" << endl;
     
     int answer;
-
+    cout << "                 '0' to back" << endl;
     cout << "                 >> ";
     cin >> answer;
-    checkChoiceFromMainMenu(answer);
+    if (answer == 0)
+    {
+        earlyScreen();
+    }
+    else
+    {
+        checkChoiceFromMainMenu(answer);
+    }
+    
 }
 
-void connecToDatabase()
+void connectToDatabase()
 {
     connection = mysql_init(0);
     connection = mysql_real_connect(connection, "localhost", "root", "abc123", "Library", 3306, NULL, 0);
@@ -49,8 +248,8 @@ void connecToDatabase()
 
 bool authenticate(string email, string password)
 {   
-    string querry = "SELECT * FROM USERS";
-    const char* q = querry.c_str();
+    query = "SELECT * FROM USERS WHERE ROLE = 'ROLE_USER'";
+    const char* q = query.c_str();
     int qstate = mysql_query(connection, q);
     if (!qstate)
     {
@@ -60,6 +259,7 @@ bool authenticate(string email, string password)
         {
             if (row[3] == email && row[4] == password)
             {   
+                id_user = *row[0] - '0';
                 return true;
             }
         }
@@ -105,8 +305,8 @@ void signIn() {
 }
 
 
-
-void checkChoiceFromMainMenu(int choice) {
+void checkChoiceFromMainMenu(int choice) 
+{
     switch (choice) 
     {
         case 1:
@@ -121,9 +321,8 @@ void checkChoiceFromMainMenu(int choice) {
     }
 }
 
-
-
-void loggedUser() {
+void loggedUser() 
+{
     cout << "\n\n";
     cout << "                 ---------------------------------------------------" << endl;
     cout << "                 ----    Welcome in Library Managment System    ----" << endl;
@@ -139,9 +338,191 @@ void loggedUser() {
     cout << "                 ----                                           ----" << endl;
     cout << "                 ----                                           ----" << endl;
     cout << "                 ---------------------------------------------------" << endl;
+
+    int choice;
+    cout << "                 >> ";
+    cin >> choice;
+    checkChoiceFromLoggedUser(choice);
+}
+void searchBookByName()
+{
+    string name;
+    
+    cout << endl;
+    cout << "                 ---------------------------------------------------" << endl;
+    cout << "                 ----               Searching Book              ----" << endl;
+    cout << "                 ---------------------------------------------------" << endl;
+    cout << "                 >> ";
+
+    /*cin >> name;*/
+    cin.ignore();
+    getline(cin, name);
+    query = "SELECT EXISTS(SELECT * FROM BOOKS WHERE TITLE = '" + name + "')";
+    mysql_query(connection, query.c_str());
+    MYSQL_RES* res = mysql_store_result(connection);
+    MYSQL_ROW row = mysql_fetch_row(res);
+   
+    if (*row[0] == '1')
+    {
+        query = "SELECT * FROM BOOKS WHERE TITLE = '" + name + "'";
+        mysql_query(connection, query.c_str());
+        MYSQL_RES* res = mysql_store_result(connection);
+        MYSQL_ROW row = mysql_fetch_row(res);
+
+        cout << "ID: " << row[0] << endl;
+        char choice;
+        cout << "                 Book found " << endl;
+        cout << "                 If you want borrow this book type 'y' if you want back to main menu type 'b'" << endl;
+        cout << "                 ---------------------------------------------------" << endl;
+        cout << "                 >> ";
+        cin >> choice;
+
+        if (choice == 'y')
+        {
+            borrowBook(*row[0] - '0');
+            loggedUser();
+        }
+        else if (choice == 'b')
+        {
+            loggedUser();
+        }
+    }
+    else
+    {
+        char choice;
+        cout << "                 Book not found..." << endl;
+        cout << "                 If you want search again type 'y' if you want back to main menu type 'b'" << endl;
+        cout << "                 ---------------------------------------------------" << endl;
+        cout << "                 >> ";
+        cin >> choice;
+
+        if (choice == 'y')
+        {
+            searchBookByName();
+        }
+        else if (choice == 'b')
+        {
+            loggedUser();
+        }
+    }
+
+
 }
 
-void registerUser() {
+void viewAllBorrowsBooks()
+{
+    query = "select Borrows.id_user, Books.* from (Borrows inner join Books ON Borrows.id_book = Books.id_book) where Borrows.id_user = "+to_string(id_user)+"";
+    mysql_query(connection, query.c_str());
+
+    MYSQL_RES* res = mysql_store_result(connection);
+    MYSQL_ROW row;
+
+    cout << endl;
+    while (row = mysql_fetch_row(res))
+    {
+        cout << "                 " << row[1] << " " << row[2] << " " << row[3] << " " << row[4] << endl;
+    }
+    char choice = 'n';
+    while (choice != 'b')
+    {
+        cout << "                 Type 'b' to back to main menu" << endl;
+        cout << "                 >> ";
+        cin >> choice;
+    }
+    loggedUser();
+}
+
+void checkChoiceFromLoggedUser(int choice)
+{
+    switch (choice)
+    {
+        case 1:
+            fetchAllBooks();
+            break;
+        case 2:
+            searchBookByName();
+            break;
+        case 3:
+            viewAllBorrowsBooks();
+            break;
+        case 4:
+            mainMenu();
+            break;
+    }
+}
+
+void fetchAllBooks()
+{
+    query = "SELECT * FROM BOOKS";
+
+    const char* q = query.c_str();
+    int qstate = mysql_query(connection, q);
+    
+    if (qstate == 0) {
+        MYSQL_RES* res = mysql_store_result(connection);
+        MYSQL_ROW row;
+        int i = 0;
+        while (row = mysql_fetch_row(res))
+        {
+            cout << "                 " << i+1 << ") "  << row[1] << " " << " " << row[2] << " " << row[3] << " " << row[4] << " " << row[5] << endl;
+            i++;
+        }
+        cout << endl;
+        cout << "               If you want to borrow type number of book otherwise press 'b' to back to menu" << endl;
+        cout << "                 >> ";
+        char choice;
+        cin >> choice;
+        while ((choice - '0' > i || choice - '0' < 0)) 
+        {
+            if (choice == 'b')
+            {   
+                loggedUser();
+                break;
+            }
+            cout << "                 >> ";
+            cin >> choice;
+        }
+        if (checkCountOfBook(choice - '0'))
+        {
+            cout << "                 Book available..." << endl;
+            borrowBook(choice - '0');
+            fetchAllBooks();
+        }
+        else
+        {   
+            cout << "                 Book not available " << endl;
+            fetchAllBooks();
+        }
+    }
+}
+
+bool checkCountOfBook(int id_book)
+{
+    query = "SELECT * FROM BOOKS WHERE ID_BOOK = '"+to_string(id_book)+"'";
+    mysql_query(connection, query.c_str());
+
+    MYSQL_RES* res = mysql_store_result(connection);
+    MYSQL_ROW row = mysql_fetch_row(res);
+    if ((int) row[5] > 0)
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void borrowBook(int id_book)
+{
+    query = "INSERT INTO BORROWS(id_user, id_book) VALUES('"+to_string(id_user)+"', '"+to_string(id_book)+"')";
+    mysql_query(connection, query.c_str());
+
+    query = "UPDATE BOOKS SET COUNTBOOKS = COUNTBOOKS - 1 WHERE ID_BOOK = '"+to_string(id_book)+"'";
+    mysql_query(connection, query.c_str());
+}
+
+void registerUser() 
+{
     system("CLS");
     string firstName, lastName, email, password, phoneNumber;
     cout << "\n\n";
@@ -180,10 +561,8 @@ void registerUser() {
     saveUserInDatabase(user);
 }
 
-void saveUserInDatabase(User user) {
-    MYSQL *connection;
-    connection = mysql_init(0);
-    connection = mysql_real_connect(connection, "localhost", "root", "abc123", "Library", 3306, NULL, 0);
+void saveUserInDatabase(User user) 
+{
     cout << "\n\n";
 
     cout << "                 Checking the connection.\n";
@@ -208,24 +587,18 @@ void saveUserInDatabase(User user) {
         Sleep(900);
         system("CLS");
 
-        string query = "INSERT INTO USERS(FirstName, LastName, Email, Password, PhoneNumber) VALUES('"+user.getFirstName()+"', '"+user.getLastName()+"', '"+user.getEmail()+"', '"+user.getPassword()+"', '"+user.getPhoneNumber()+"')";
-        cout << query;
+        query = "INSERT INTO USERS(FirstName, LastName, Email, Password, PhoneNumber, Role) VALUES('"+user.getFirstName()+"', '"+user.getLastName()+"', '"+user.getEmail()+"', '"+user.getPassword()+"', '"+user.getPhoneNumber()+"', '"+user.getRole()+"')";
         mysql_query(connection, query.c_str());
+        signIn();
     }
 }
 
 
 
 
-
-
-
 int main()
 {
-    connecToDatabase();
-    mainMenu();
-    
-
-
+    connectToDatabase();
+    earlyScreen();
 }
 
